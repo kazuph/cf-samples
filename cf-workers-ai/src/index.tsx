fdfcba31c4416a790e35ce4d9e4812264942e53d
Kazuhiro Homma <kazu.homma@gmail.com>
2023-10-11 07:01:34 +0900

    stream対応

--- cf-workers-ai/src/index.tsx
+++ cf-workers-ai/src/index.tsx
@@ -32,6 +32,15 @@ app.get('/', (c) => {
   )
 })
 
+app.get('/stream', (c) => {
+  return c.streamText(async (stream) => {
+    stream.writeln('Hello')
+    await stream.sleep(1000)
+    stream.writeln('Hono!')
+  })
+})
+
+
 app.get('/ai', async (c) => {
   const ai = new Ai(c.env.AI)
   const answer: Answer = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
@@ -43,6 +52,27 @@ app.get('/ai', async (c) => {
     ]
   })
   return c.text(answer.response)
+})
+
+app.get('/stream_ai', async (c) => {
+  const ai = new Ai(c.env.AI)
+  const answer: Answer = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+    messages: [
+      {
+        role: 'user',
+        content: `What is Cloudflare Workers. You respond in less than 100 words.`
+      }
+    ]
+  })
+
+  const strings = [...answer.response]
+
+  return c.streamText(async (stream) => {
+    for (const s of strings) {
+      stream.write(s)
+      await stream.sleep(10)
+    }
+  })
 })
 
 export default app
