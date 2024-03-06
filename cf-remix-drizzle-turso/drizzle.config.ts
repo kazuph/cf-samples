30aaf4a496843ac9967fd80b6abab1974399f46f
Kazuhiro Homma <kazu.homma@gmail.com>
2024-03-06 16:10:49 +0900

    fix drizzle.config.ts

--- cf-remix-drizzle-turso/drizzle.config.ts
+++ cf-remix-drizzle-turso/drizzle.config.ts
@@ -1,16 +1,15 @@
 import * as dotenv from "dotenv";
 
-// 環境に応じて.envファイルを読み込む
-dotenv.config({ path: process.env.NODE_ENV === "production" ? ".env.prod" : ".env" });
+dotenv.config({ path: process.env.NODE_ENV === "production" ? ".env.prod" : ".dev.vars" });
 
 export default {
   schema: "./app/db/schema.ts",
   out: "./db/migrations",
-  driver: process.env.DB_DRIVER,
-  dbCredentials: process.env.DB_DRIVER === "turso" ? {
+  driver: process.env.DB_DRIVER as string,
+  dbCredentials: process.env.DB_DRIVER === "turso" ? { // prod = turso, dev = libsql
     url: process.env.TURSO_DB_URL as string,
     authToken: process.env.TURSO_DB_AUTH_TOKEN as string,
   } : {
-    database: process.env.DB_PATH as string,
+    url: process.env.TURSO_DB_URL as string,
   },
 };
