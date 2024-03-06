46f26234f1d34f7f9d96877fa3e980dc15b9b6d1
Kazuhiro Homma <kazu.homma@gmail.com>
2024-03-06 17:29:46 +0900

    schemaが変だったので修正

--- cf-remix-drizzle-turso/app/db/schema.ts
+++ cf-remix-drizzle-turso/app/db/schema.ts
@@ -5,7 +5,8 @@ export const users = sqliteTable('users', {
   id: text('id').primaryKey(),
   email: text('email').unique(),
   name: text('name').notNull(),
+  nickname: text('nickname'),
   customerId: text('customerId').unique(),
-  createdAt: text("timestamp").default(sql`CURRENT_TIMESTAMP`),
-  updatedAt: text("timestamp").default(sql`CURRENT_TIMESTAMP`),
+  createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
+  updatedAt: text("updated_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
 });
---
+++ cf-remix-drizzle-turso/db/migrations/0001_bored_cassandra_nova.sql
@@ -0,0 +1,11 @@
+ALTER TABLE `users` RENAME COLUMN `timestamp` TO `created_at`;--> statement-breakpoint
+/*
+ SQLite does not support "Set not null to column" out of the box, we do not generate automatic migration for that, so it has to be done manually
+ Please refer to: https://www.techonthenet.com/sqlite/tables/alter_table.php
+                  https://www.sqlite.org/lang_altertable.html
+                  https://stackoverflow.com/questions/2083543/modify-a-columns-type-in-sqlite3
+
+ Due to that we don't generate migration automatically and it has to be done manually
+*/--> statement-breakpoint
+ALTER TABLE users ADD `nickname` text;--> statement-breakpoint
+ALTER TABLE users ADD `updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL;
