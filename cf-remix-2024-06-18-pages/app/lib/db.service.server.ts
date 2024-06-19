9060b9fdbcc0fa60ade726cb2283f466e8dc1b6b
Kazuhiro Homma <kazu.homma@gmail.com>
2024-06-19 14:47:04 +0900

    chore: Update context structure to support new database configuration

--- cf-remix-2024-06-18-pages/app/lib/db.service.server.ts
+++ cf-remix-2024-06-18-pages/app/lib/db.service.server.ts
@@ -1,19 +1,24 @@
 import { and, eq, gt } from "drizzle-orm";
-import type { DrizzleD1Database } from "drizzle-orm/d1";
+import type { DrizzleD1Database, D1Database } from "drizzle-orm/d1";
 import { drizzle } from "drizzle-orm/d1";
 
-// const contextWithDb = (
-// 	context: Record<string, unknown>,
-// ): context is { DB: D1Database } => {
-// 	return "DB" in context;
-// };
+// contextの構造が変わったため、新しい構造に対応させます。
+const contextWithDb = (
+	context: Record<string, unknown>,
+): context is { cloudflare: { env: { DB: D1Database } } } => {
+	return (
+		"cloudflare" in context &&
+		"env" in context.cloudflare &&
+		"DB" in context.cloudflare.env
+	);
+};
 
 export const getDbFromContext = (
 	context: Record<string, unknown>,
 ): DrizzleD1Database => {
-	// if (!contextWithDb(context)) {
-	// 	throw new Error("No database in context");
-	// }
+	if (!contextWithDb(context)) {
+		throw new Error("No database in context");
+	}
 
 	return drizzle(context.cloudflare.env.DB);
 };
